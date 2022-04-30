9f629544e5bb7323b3dae760c813f1ef9e83dc72
Will Silva <lacroix-dieu@hotmail.com>
2022-04-30 01:13:59 +0000

    Update ShowExplanation.cs

--- Assets/Scripts/ShowExplanation.cs
+++ Assets/Scripts/ShowExplanation.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 
+// Simple logic to hide and show the UI explanation on contact with the player
 public class ShowExplanation : MonoBehaviour
 {
+    // Which object to manipulate
     public GameObject effectorExplanation;
 
+    // Guarantee that the object will begin deactivated
     private void Awake()
     {
         effectorExplanation.SetActive(false);
     }
 
+    // On trigger with the player, activate the object
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -17,6 +21,7 @@ public class ShowExplanation : MonoBehaviour
         }
     }
 
+    // On trigger exit with the player, deactivate the object
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))

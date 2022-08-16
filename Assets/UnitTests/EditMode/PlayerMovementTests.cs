17fca5b2ea06113798f0599c9ed9fe2d589a9441
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-08-16 05:19:01 +0000

    Added first steps for testing.

---
+++ Assets/UnitTests/EditMode/PlayerMovementTests.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayerMovementTests
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void RightMovement()
+    {
+        // Use the Assert class to test conditions
+    }
+}
